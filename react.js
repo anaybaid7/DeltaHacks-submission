dbdc66e932d3c9d1d0f1464360042ffa9613eb2e
Anay Baid <anaybaid7@gmail.com>
2023-01-15 03:26:37 -0500

    Add files via upload
    react.js

---
+++ react.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+import { useForm } from 'react-hook-form';
+import { useSelector, useDispatch } from 'react-redux';
+import { addTodo, updateTodo } from './actions/todoActions';
+
+import './App.css';
+
+function App() {
+  const { register, handleSubmit, errors } = useForm();
+  const dispatch = useDispatch();
+  const todos = useSelector(state => state.todos);
+
+  const onSubmit = (data) => {
+    if (!data.id) {
+      dispatch(addTodo(data));
+    } else {
+      dispatch(updateTodo(data));
+    }
+  };
+
+  return (
+    <Router>
+      <div className="App">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/completed">Completed</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/completed">
+            <CompletedTodos todos={todos} />
+          </Route>
+          <Route path="/">
+            <TodoForm
+              register={register}
+              handleSubmit={handleSubmit}
+              onSubmit={onSubmit}
+              errors={errors}
+            />
+            <Todos todos={todos} />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
